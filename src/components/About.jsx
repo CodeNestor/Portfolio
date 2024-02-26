b03da218c07cca5a98d64ad7dfa576f1a291d300
AhmadHassanMughal <ahmadhassa2003@gmail.com>
2024-02-26 16:07:10 +0500

    changes in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,14 +14,14 @@ const About = () => {
         </div>
 
         <p className="text-xl mt-5">
-        Experienced Frontend Developer with a 1.5 year of hands-on
+        Experienced Frontend Developer with a 2 year of hands-on
         expertise in HTML,CSS, and JavaScript, specializing in responsive
         design for seamless user interactions. Adept at leveraging React.js,
         Next.js, and Tailwind CSS to create visually striking and userfriendly
         web applications. Proven track record of translating design concepts
         into pixel-perfect, responsive interfaces, and enhancing user
         engagement. Ready to bring my skills and passion for innovative
-        frontend development to your team
+        frontend development to your team.
         {/* I am a web developer, working as a frontend developer. Excited for improving my skills and learning new technologies. I'm open to learn and work with any web technology. */}
         </p>
 
